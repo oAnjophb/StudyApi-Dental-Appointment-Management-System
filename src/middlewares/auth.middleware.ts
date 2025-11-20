82d7cee7043af5cdced9edcb0eb7f224bb9b8b95
Ruan Anjos <anjosruanp@gmail.com>
2025-11-20 13:27:46 -0300

    feat: implement auth middleware and type definitions" -m "- Create ensureAuthenticated middleware to validate JWT - Extend Express Request type to include user_id - Protect GET /users route requiring valid token - Handle 401 unauthorized scenarios

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Ipayload {
+  sub: string;
+}
+
+export function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authToken = req.headers.authorization;
+
+  if (!authToken)
+    return res.status(401).json({
+      error: "Token is missing",
+      code: "token.missing",
+    });
+  const [, token] = authToken.split(" ");
+
+  try {
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) throw new Error("JWT_SECRET not found env");
+
+    const { sub } = verify(token, secret) as Ipayload;
+    req.user_id = sub;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({
+      error: "Token invalid or expired",
+      code: "token.expired",
+    });
+  }
+}
--- src/routes/v1/user.routes.ts
+++ src/routes/v1/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { UserController } from "../../controllers/v1/user.controller";
+import { ensureAuthenticated } from "../../middlewares/auth.middleware";
 
 const userRoutes = Router();
 const userController = new UserController();
 
 userRoutes.post("/", userController.createUser);
-userRoutes.get("/", userController.getAllUsers)
+userRoutes.get("/", ensureAuthenticated, userController.getAllUsers);
 
 export default userRoutes;
