d33e075a4f9df8c83a723ff5e07269a9c4e20227
Ruan Anjos <anjosruanp@gmail.com>
2025-11-30 20:57:25 -0300

    fix: correct import case for FormFooter component in LoginForm

--- Ui/src/components/forms/LoginForms.tsx
+++ Ui/src/components/forms/LoginForms.tsx
@@ -1,12 +1,13 @@
 import useLoginForm from "../../hooks/useLoginForm";
-import FormFooter from "../layout/formFooter";
+import FormFooter from "../layout/FormFooter";
 import Button from "../ui/Button";
 import Input from "../ui/Input";
 
 import { Mail, Lock } from "lucide-react";
 
 function LoginForm() {
-  const { email, setEmail, password, setPassword, handleSubmit } = useLoginForm();
+  const { email, setEmail, password, setPassword, handleSubmit } =
+    useLoginForm();
 
   return (
     <form onSubmit={handleSubmit} className="space-y-6">
